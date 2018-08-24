e2a90af4e5872d83e6dbe259e491a41c8c614a0f
Dennis <Dennis'@Dennis-MBP.local>
2018-08-24 11:12:44 -0700

    now collects list of categories, and persists the list in localStorage

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let quote = document.getElementById("quote");
 let author = document.getElementById("author");
+let cat = document.getElementById("cat");
+let isList = JSON.parse(localStorage.getItem("list"));
+let catList = isList ? isList : [];
 
 let getQuote = () => {
   fetch("https://talaikis.com/api/quotes/random/")
@@ -7,9 +10,17 @@ let getQuote = () => {
     .then(data => {
       quote.innerHTML = data.quote;
       author.innerHTML = data.author;
+      cat.innerHTML = data.cat;
+      addToCatList(data.cat);
     });
 };
-
 getQuote();
 
 document.getElementById("quoteButton").addEventListener("click", getQuote);
+
+const addToCatList = newCat => {
+  if (!catList.includes(newCat)) {
+    catList.push(newCat);
+    localStorage.setItem("list", JSON.stringify(catList));
+  }
+};
